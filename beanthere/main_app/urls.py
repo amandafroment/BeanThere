99997bb897c0c784db6b16a98817223846b1b333
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-11-10 13:03:47 -0700

    GROUP: Create initial url paths and views

--- beanthere/main_app/urls.py
+++ beanthere/main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing'),
+    path('', views.landing, name='landing'), # Login screen
+    path('home/', views.home, name='home'), # Initial search page
+    path('index/', views.index, name='index'), # Search Results
+    path('details/', views.details, name='details'), # Individual Cafe Details
+    path('user/', views.user, name='user'), # User Profile
+    path('review/', views.review, name='review'), # Review Form
 ]
--- beanthere/main_app/views.py
+++ beanthere/main_app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Define the home view
-def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
 # Create your views here.
 def landing(request):
     return HttpResponse('<h1>Landing Page</h1>')
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Home</h1>')
+
+# Define the home view
+def index(request):
+  return HttpResponse('<h1>Index</h1>')
+
+# Define the details view
+def details(request):
+  return HttpResponse('<h1>Details</h1>')
+
+# Define the user view
+def user(request):
+  return HttpResponse('<h1>Users</h1>')
+
+# Define the review view
+def review(request):
+  return HttpResponse('<h1>Reviews</h1>')
