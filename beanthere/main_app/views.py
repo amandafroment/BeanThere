9c6f89b45f36e3d30e6cb45b94b5edba70057110
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-11-10 12:54:13 -0700

    GROUP: Babys first landing page

--- beanthere/main_app/views.py
+++ beanthere/main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 # Create your views here.
+def landing(request):
+    return HttpResponse('<h1>Landing Page</h1>')
