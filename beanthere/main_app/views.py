fd3a043592320be7914c99e210841cb8189edcc0
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-11-14 11:09:24 -0700

    Implement API request/response

--- beanthere/main_app/views.py
+++ beanthere/main_app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth import login
+import os
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+from urllib.error import HTTPError
+from urllib.parse import quote
+from urllib.parse import urlencode
+
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+
+API_KEY = os.environ['YELP_KEY'] 
+
+DEFAULT_TERM = 'coffee'
+DEFAULT_LOCATION = 'Toronto'
+SEARCH_LIMIT = 3
 
 # Create your views here.
 def landing(request):
@@ -14,7 +35,10 @@ def home(request):
 
 # Define the home view
 def index(request):
-  return render(request, 'users/index.html')
+  response_data = api_search(API_KEY, DEFAULT_TERM, DEFAULT_LOCATION)
+  search_data = response_data.get('businesses')
+  print(search_data)
+  return render(request, 'users/index.html', 'data': search_data)
 
 # Define the details view
 def details(request):
@@ -27,3 +51,25 @@ def user(request):
 # Define the review view
 def review(request):
   return render(request, 'users/review.html')
+
+
+def api_search(api_key, term, location):
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return api_request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+def api_request(host, path, api_key, url_params=None):
+  url_params = url_params or {}
+  url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+  headers = {
+    'Authorization': 'Bearer %s' % api_key,
+  }
+
+  print(u'Querying {0} ...'.format(url))
+
+  response = requests.request('GET', url, headers=headers, params=url_params)
+
+  return response.json()
